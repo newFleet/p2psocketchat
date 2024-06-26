7fbcc90f7c10b74e1870321810bdf1743fec77a7
Rono <63807776+Rono0365@users.noreply.github.com>
2024-06-26 11:19:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const server = app.listen(3000, () => {
 const xx =[];
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 // Configure multer to handle file uploads
 const storage = multer.diskStorage({
